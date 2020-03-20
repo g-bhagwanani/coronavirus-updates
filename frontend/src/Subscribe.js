4d11d24c2b465788349f07c15716d30a8de2359b
Gaurav Bhagwanani <gaurav.bhagwanani@gmail.com>
2020-03-21 05:07:20 +0530

    validation of sub. form done, frontend shows appropriate message

--- frontend/src/Subscribe.js
+++ frontend/src/Subscribe.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 import './Subscribe.css';
-import { Button, Form, FormGroup, Label, Input, Container, Col } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, Container, Col, Alert } from 'reactstrap';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 class Subscribe extends Component {
 
     constructor(props) {
         super(props);
         this.action_url = window.location.protocol + '//' + window.location.hostname +":5000/subscribe";
-
     }
 
     render() {
+
+        var SuccessAlert = <></>;
+        var DangerAlert = <></>;
+        if (this.props.message === 'subscribed') {
+            SuccessAlert = <Alert color="success">You will now recieve regular coronavirus updates on your email id!</Alert>;
+        }
+        else if (this.props.message === 'wrongemail') {
+            DangerAlert = <Alert color="danger">The email id that you entered is incorrect, please try again</Alert>;
+        }
+        else if (this.props.message === 'incomplete') {
+            DangerAlert = <Alert color="danger">Please fill all the fields to subscribe successfully</Alert>;
+        }
+
         return (
             <Container className="form_container">
                 <Col>
-                    <p>Subscribe to our service to get regular updates about coronavirus statistics in your country:</p>
+                    {SuccessAlert}
+                    {DangerAlert}
+                    <h6>Subscribe to our service to get regular updates about coronavirus statistics in your country.</h6>
                 </Col>
                 <Form className="form" action={this.action_url} method="post">
                     <Col>
@@ -39,6 +54,10 @@ class Subscribe extends Component {
                         <Button type="submit" color="dark" className="btn">Subscribe</Button>
                     </Col>
                 </Form>
+                <br></br>
+                <Col className="text-center">
+                <Link to = "/stats"><Button color="danger" className="btn">Continue without subscribing</Button></Link>
+                </Col>
             </Container>
         );
     }
